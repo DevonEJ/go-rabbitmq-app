7029e90ae68d1fb6b6f7ea19cd15aab9c5472b46
Devs <devon.edwardsjoseph@U-SH-HO-MAC88.local>
2019-12-01 19:52:16 +0000

    Correcting logic in sensor reading generation

--- src/distributed/sensors/sensors.go
+++ src/distributed/sensors/sensors.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"log"
 	"math/rand"
 	"time"
 	"strconv"
@@ -14,17 +15,16 @@ var max = flag.Float64("max", 5., "maximum val for reading")
 var min = flag.Float64("min", 1., "min val for reading")
 var stepSize = flag.Float64("step", 0.1, "max allowable diff between readings")
 
+// Create input data - use nanosecond seed to ensure unique
+var ran = rand.New(rand.NewSource(time.Now().UnixNano()))
+var reading = ran.Float64()*(*max-*min) + *min
+var nom = (*max - *min) / 2 + *min
+
+
 
 func main() {
-
 	// Parse command line flags for sensors
 	flag.Parse()
-
-	// Create input data - use nanosecond seed to ensure unique
-	var ran = rand.New(rand.NewSource(time.Now().UnixNano()))
-
-	var reading = ran.Float64()*(*max-*min) + *min
-
 
 	// Create duration and signal from desired frequency of reading recording
 	duration , _ := time.ParseDuration(strconv.Itoa(1000/int(*freq)) + "ms")
@@ -32,13 +32,23 @@ func main() {
 
 	for range signal {
 		calcReading()
+		log.Printf("Reading sent: %v \n", reading)
 
 	}
-
-
 }
 
 
 func calcReading() {
-	
+
+	var maxStep, minStep float64
+
+	if reading < nom {
+		maxStep = *stepSize
+		minStep = -1 * *stepSize * (reading - *min) / (nom - *min)
+	} else {
+		maxStep = *stepSize * (*max - reading) / (*max - nom)
+		minStep = -1 * *stepSize
+	}
+
+	reading += ran.Float64() * (maxStep - minStep) + minStep
 }

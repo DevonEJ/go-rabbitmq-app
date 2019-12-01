92e282f0b97fc5e675463fd8bfa1f23e5df35b46
Devs <devon.edwardsjoseph@U-SH-HO-MAC88.local>
2019-12-01 19:25:05 +0000

    Created sensors package to collect data for messages

--- src/distributed/sensors/sensors.go
+++ src/distributed/sensors/sensors.go
@@ -1,1 +1,20 @@
-package sensors
+package main
+
+import (
+	"flag"
+)
+
+// Capture details desired sensor from cli flags
+var name = flag.String("name", "sensor", "name of sensor")
+var freq = flag.Uint("freq", 5, "update frequency in cycles/sec")
+var max = flag.Float64("max", 5., "maximum val for reading")
+var min = flag.Float64("min", 1., "min val for reading")
+var stepSize = flag.Float64("step", 0.1, "max allowable diff between readings")
+
+
+func main() {
+
+	// Parse command line flags for sensors
+	flag.Parse()
+
+}
